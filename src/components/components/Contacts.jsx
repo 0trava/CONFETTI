d040fe5f4b6737af07acb6c845ba4e0961341186
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-02-04 00:01:16 +0200

    add contact

--- src/components/components/Contacts.jsx
+++ src/components/components/Contacts.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import style from './Contacts.module.css';
+import Forma from './Forma';
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <div className={style.container}>
+
+      <div className={style.contact_box}>
+      {/* title */}
+      <h2 className={style.title}>Kontakty</h2>
+
+      <p className={style.post_title}>Confetti - Studio<br/>
+        Dekoracij Balonami<br/>
+        Warszawa </p>
+
+      {/* Contacts */}
+      <ul className={style.contact_list}>
+        <li><p>+48 793 351 407</p></li>
+        <li><p>confettibalony@gmail.com</p></li>
+      </ul>
+
+      </div>
+
+      {/* FORM BLOCK */}
+      <div className={style.form_block}>
+        <div className={style.ballon1}></div>
+        <div className={style.ballon2}></div>
+        <div className={style.form_block}>
+          <Forma />
+        </div>
+        
+      </div>
+
+
+
+
+
+
+
+    </div>
   )
 }
 
