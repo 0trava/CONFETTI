7c0cc38a7e5bd9c24274b142d1fc32acf42201e0
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-02-03 21:27:56 +0200

    add BurgerMenu text

--- src/components/components/BurgerMenu.jsx
+++ src/components/components/BurgerMenu.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import styles from './BurgerMenu.module.css';
+import style from './BurgerMenu.module.css';
+
+// Images
+import Logo from '../../assets/images/logo_menu.png';
+import BtnClose from '../../assets/images/menu_btn_close.png';
 
 const BurgerMenu = ({ toggleBurgerMenu }) => {
   return (
-    <div className={styles['burger-menu']}>
-      <button className={styles['close-button']} onClick={toggleBurgerMenu}>
-        &times;
-      </button>
+    <div className={style.burger_menu}>
+
+      <div className={style.header} >
+          {/* logo */}
+            <a href="/" className={style.logo_link}>
+              <img className={style.logo} src={Logo} alt="logo" />
+            </a>
+        
+
+          {/* Btn close */}
+          <button className={style.close_button} onClick={toggleBurgerMenu}>
+              <img src={BtnClose} alt="BtnClose" />
+          </button>
+
+      </div>
+
+      <ul className={style.list}>
+            <li>główna</li>
+            <li>O nas</li>
+            <li>Recenzje</li>
+            <li>Kontakty</li>
+      </ul>
+
+
       {/* Ваш вміст меню */}
     </div>
   );
