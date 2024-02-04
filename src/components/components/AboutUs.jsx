e19c7eb9fb59ae85806ae80ac1e5d71c5c1ceec3
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-02-04 19:55:01 +0200

    add style for dot in carousel

--- src/components/components/AboutUs.jsx
+++ src/components/components/AboutUs.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import style from './AboutUs.module.css';
 
+// React Responsive Carousel
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+
+
+
 const AboutUs = () => {
   return (
     <div className={style.container}>
@@ -16,12 +23,12 @@ const AboutUs = () => {
 
       <div className={style.list}>
         {/* 1 */}
-        <div class={style.item}>
-          <h2 class={style.item__title}>
+        <div className={style.item}>
+          <h2 className={style.item__title}>
             Kreatywne podejście i<br />
             dbałość o szczegóły
           </h2>
-          <p class={style.item__text}>
+          <p className={style.item__text}>
             Specjaliści zapewniają doradztwo i<br />
             kreatywność przy wyborze dekoracji
             <br />
@@ -31,13 +38,13 @@ const AboutUs = () => {
         </div>
 
         {/* 2 */}
-        <div class={style.item}>
-          <h2 class={style.item__title}>
+        <div className={style.item}>
+          <h2 className={style.item__title}>
             Materiały przyjazne
             <br />
             dla środowiska
           </h2>
-          <p class={style.item__text}>
+          <p className={style.item__text}>
             Wykorzystywane są materiały najwyższej jakości, w tym
             biodegradowalne materiały balonowe, aby pozostawić jak najmniejszy
             ślad na środowisku.
@@ -45,25 +52,25 @@ const AboutUs = () => {
         </div>
 
         {/* 3 */}
-        <div class={style.item}>
-          <h2 class={style.item__title}>
+        <div className={style.item}>
+          <h2 className={style.item__title}>
             Szeroka gama kolorów
             <br />
             do personalizacji
           </h2>
-          <p class={style.item__text}>
+          <p className={style.item__text}>
             Balony i dekoracje są wybierane z szerokiej gamy kolorów, co pozwala
             spersonalizować wystrój na każdą okazję.
           </p>
         </div>
 
         {/* 4 */}
-        <div class={style.item}>
-          <h2 class={style.item__title}>
+        <div className={style.item}>
+          <h2 className={style.item__title}>
             Gwarancja dostawy
             <br />i montażu
           </h2>
-          <p class={style.item__text}>
+          <p className={style.item__text}>
             Firma gwarantuje nie tylko wysokiej jakości dostawę zamówień, ale
             także ich montaż, zapewniając klientom kompleksową obsługę.
           </p>
@@ -71,13 +78,90 @@ const AboutUs = () => {
       </div>
 
       {/* carousel -------------------------------------- */}
-      {/* <div className={style.slider_section}>
-      <div className={style.slider_container}>
-        <div className={style.slider_item}>1</div>
-        <div className={style.slider_item}>2</div>
-        <div className={style.slider_item}>3</div>
+      <div className={`${style.carouselCont}`}>
+      <Carousel
+        className={`${style.carouselContainer} ${style.customCarousel}`}
+        showArrows={false}
+        showStatus={false}
+        style={{ overflow: 'visible' }}
+        renderIndicator={(onClickHandler, isSelected, index, label) => {
+          const indicatorClass = isSelected ? 'dot selected' : 'dot';
+          const dotStyle = {
+            margin: '-50px 9px',
+            backgroundColor: isSelected ? '#222222' : 'rgba(34, 34, 34, 1)',
+          };
+  
+          return (
+            <li
+            className={indicatorClass}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+            value={index}
+            key={index}
+            role="button"
+            tabIndex={0}
+            title={`${label} ${index + 1}`}
+            aria-label={`${label} ${index + 1}`}
+            style={dotStyle}
+          />
+          );
+        }}
+      >
+        {/* 1 */}
+        <div className={style.item}>
+          <h2 className={style.item__title}>
+            Kreatywne podejście i<br />
+            dbałość o szczegóły
+          </h2>
+          <p className={style.item__text}>
+            Specjaliści zapewniają doradztwo i<br />
+            kreatywność przy wyborze dekoracji
+            <br />
+            balonowych, z dbałością o szczegóły
+            <br />
+          </p>
+        </div>
+
+        {/* 2 */}
+        <div className={`${style.item} ${style.item_2}`}>
+          <h2 className={style.item__title}>
+            Materiały przyjazne
+            <br />
+            dla środowiska
+          </h2>
+          <p className={style.item__text}>
+            Wykorzystywane są materiały najwyższej jakości, w tym
+            biodegradowalne materiały balonowe, aby pozostawić jak najmniejszy
+            ślad na środowisku.
+          </p>
+        </div>
+
+        {/* 3 */}
+        <div className={`${style.item} ${style.item_3}`}>
+          <h2 className={style.item__title}>
+            Szeroka gama kolorów
+            <br />
+            do personalizacji
+          </h2>
+          <p className={style.item__text}>
+            Balony i dekoracje są wybierane z szerokiej gamy kolorów, co pozwala
+            spersonalizować wystrój na każdą okazję.
+          </p>
+        </div>
+
+        {/* 4 */}
+        <div className={`${style.item} ${style.item_4}`}>
+          <h2 className={style.item__title}>
+            Gwarancja dostawy
+            <br />i montażu
+          </h2>
+          <p className={style.item__text}>
+            Firma gwarantuje nie tylko wysokiej jakości dostawę zamówień, ale
+            także ich montaż, zapewniając klientom kompleksową obsługę.
+          </p>
+        </div>
+      </Carousel>
       </div>
-    </div> */}
     </div>
   );
 };
