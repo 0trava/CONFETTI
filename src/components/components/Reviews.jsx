c3941db6c20707b06e082c4862b4785d566a345c
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-02-04 17:14:55 +0200

    add Reviews

--- src/components/components/Reviews.jsx
+++ src/components/components/Reviews.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import style from './Reviews.module.css';
+
+// images
+
+// import Emojie from '../../assets/images/emojie.png'
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+    <div className={style.container}>
+        <h2 className={style.title}>
+        Recenzje
+        </h2>
+
+        {/*Reviews Desktop*/}
+        <ul className={style.list__desk}>
+          <li className={style.item__desk}>
+              <p  className={style.item__review}>Baaardzo super ścianka  , dziękuję za <span>współpracę</span></p>
+              <p className={style.item__user}>Anetta Ostalczyk </p>
+          </li>
+
+          <li className={style.item__desk}>
+              <p  className={style.item__review}>Jeszcze raz wielkie dzięki! Ciężko nam było foty robić jak ustawili te stoły 🥰 ale sam klimat i dekoracja robiła meegaaa wrażenie!  </p>
+              <p className={style.item__user}>Aleksandra Murawska</p>
+          </li>
+
+          <li className={style.item__desk}>
+              <p  className={style.item__review}>Ścianka  była piękna , bardzo delikatna , Jeszcze raz bardzo dziękuję  ) </p>
+              <p className={style.item__user}>Katia Maksymiw </p>
+          </li>
+        </ul>
+
+    </div>
   )
 }
 
