d1fd224daebd5e375131e8bbdd26dca2d131bdd4
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-02-04 17:29:06 +0200

    add scroll burger menu

--- src/components/components/Header.jsx
+++ src/components/components/Header.jsx
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './Header.module.css';
 
 // Images
 import Logo from '../../assets/images/logo.png';
 import MenuFull from '../../assets/images/menu.png';
 import MenuFull_hover from '../../assets/images/menu_hover.png';
-// import MenuIcon from '../../assets/images/menu_icon.png';
+import MenuIcon from '../../assets/images/menu_icon.png';
+import MenuIcon_hover from '../../assets/images/menu_icon_hover.png';
 
-const Header = ({ isScrolled, toggleBurgerMenu }) => {
-  console.log(isScrolled);
+const Header = ({ toggleBurgerMenu }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      setIsScrolled(offset > 50);
+    };
 
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className={style.container}>
-            {/* Logo */}
-            <a href="/" className={style.logo_link}>
-              <img className={style.logo} src={Logo} alt="logo" />
-            </a>
+    <div className={`${style.container} ${isScrolled ? style.scrolled : ''}`}>
+      {/* Logo */}
+      {!isScrolled ? (<a href="/" className={style.logo_link}>
+        <img className={style.logo} src={Logo} alt="logo" />
+      </a>) : (<div/>)}
 
-
+      {/* Menu */}
+      <div onClick={toggleBurgerMenu} className={style.menu__full_box}>
+        {isScrolled ? (
+          <>
+            {/* Menu scroll*/}
+            <img className={style.menu} src={MenuIcon} alt="MenuFull icon" />
+            <img className={style.menu_hover} src={MenuIcon_hover} alt="MenuFull icon hover" />
+          </>
+        ) : (
+          <>
             {/* Menu */}
-            <div onClick={toggleBurgerMenu} className={style.menu__full_box}>
-               <img className={style.menu} src={MenuFull} alt="MenuFull icon" />
-               <img className={style.menu_hover} src={MenuFull_hover} alt="MenuFull icon hover" />
-            </div>
+            <img className={style.menu} src={MenuFull} alt="MenuFull icon" />
+            <img className={style.menu_hover} src={MenuFull_hover} alt="MenuFull icon hover" />
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
